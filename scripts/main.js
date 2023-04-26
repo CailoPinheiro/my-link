e2c390ada198f5cbbdb39abb61a9769f80c0d513
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-04-26 00:52:08 -0300

    implements smooth transition to innerHTML

--- scripts/main.js
+++ scripts/main.js
@@ -23,19 +23,25 @@ const pixContent = document.getElementById("pixContent");
 const pix = "bce23200-89bc-42e8-b15c-62914cfdaa9c";
 
 function copiar() {
-  navigator.clipboard.writeText(pix)
+  navigator.clipboard
+    .writeText(pix)
     .then(swap)
-    .catch((err) => console.error('Erro ao copiar:', err));
+    .catch((err) => console.error("Erro ao copiar:", err));
 }
 
 function swap() {
-  if (navigator.clipboard && typeof navigator.clipboard.writeText === 'function') {
-    const copied = "<span id='pix'>Copiado <img src='assets/icon-check.svg' alt'check'></span>";
-    pixContent.innerHTML = copied;
-  } else {
+  if (
+    navigator.clipboard &&
+    typeof navigator.clipboard.writeText === "function"
+  ) {
     setTimeout(() => {
-      pixContent.innerHTML = "<img src='assets/icon-copy.svg' alt='copiar'><span id='pix'>Pix</span>";
-    }, 500);
+      pixContent.innerHTML =
+        "<span id='pix' class='fade-in'>Copiado <img src='assets/icon-check.svg' alt'check'></span>";
+    }, 100);
+
+    // setTimeout(() => {
+    //   pixContent.innerHTML =
+    //     "<img src='assets/icon-copy.svg' alt='copiar'><span id='pix' class='fade-in'>Pix</span>";
+    // }, 10000);
   }
 }
-
