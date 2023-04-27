0bb6f5b7b71306fddfa918d5ce91d75f70123871
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-04-27 11:48:33 -0300

    a

---
+++ scripts/sound.js
@@ -0,0 +1,17 @@
+// let links = document.querySelectorAll('#links a');
+// let sound = document.getElementsByTagName('audio');
+
+// const audio = new Audio('assets/q.mp3');
+// for (let i = 0; i < links.length; i++) {
+//     links[i].addEventListener('mouseover', () => {
+//         sound[0].currentTime = 0;
+//         sound[0].play();
+//     });
+// }
+
+// document.addEventListener('click', function() {
+//     var audio = new Audio('assets/q.mp3');
+//     audio.play();
+// });
+
+
